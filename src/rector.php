9f9d3c7a3ff146300853cae39f8dbebafab0348b
MGatner <mgatner@icloud.com>
2021-10-28 20:01:03 +0000

    Add UP_TO_PHP_74

--- src/rector.php
+++ src/rector.php
@@ -16,14 +16,20 @@ declare(strict_types=1);
 use Rector\Core\Configuration\Option;
 use Rector\Core\ValueObject\PhpVersion;
 use Rector\Php74\Rector\Property\TypedPropertyRector;
+use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
-    // get parameters
+
+    // Import set lists up to and including PHP 7.4 features
+    $containerConfigurator->import(LevelSetList::UP_TO_PHP_74);
+    $containerConfigurator->import(SetList::PHP_74);
+
+    // Get parameters
     $parameters = $containerConfigurator->parameters();
     $parameters->set(Option::PATHS, [
-        __DIR__ . '/app'
+        __DIR__ . '/app',
     ]);
 
     $parameters->set(Option::BOOTSTRAP_FILES, [
@@ -34,8 +40,11 @@ return static function (ContainerConfigurator $containerConfigurator): void {
 
     $parameters->set(Option::PHP_VERSION_FEATURES, PhpVersion::PHP_74);
 
-    // import php 7.4 set list for php 7.4 features
-    $containerConfigurator->import(SetList::PHP_74);
+    // Some opinionated rules that may merit skipping
+    $parameters->set(Option::SKIP, [
+        \Rector\Php71\Rector\FuncCall\CountOnNullRector::class,
+        \Rector\Php74\Rector\Property\RestoreDefaultNullToNullableTypePropertyRector::class,
+    ]);
 
     // get services
     $services = $containerConfigurator->services();
